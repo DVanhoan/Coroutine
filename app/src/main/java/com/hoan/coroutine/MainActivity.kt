815074806d50630cae9e4f53e568b4daacb0c1df
DVanhoan <144916089+DVanhoan@users.noreply.github.com>
2025-03-27 22:28:24 +0700

    up

--- app/src/main/java/com/hoan/coroutine/MainActivity.kt
+++ app/src/main/java/com/hoan/coroutine/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.hoan.coroutine
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import com.hoan.coroutine.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    val myViewModel:MyViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding!!.btnAdd.setOnClickListener {
+            val a = binding!!.etNumber1.text.toString().toIntOrNull()?:0
+            val b = binding!!.etNumber2.text.toString().toIntOrNull()?:0
+            myViewModel.cong(a,b)
         }
+
+        binding!!.btnSubtract.setOnClickListener {
+            val a = binding!!.etNumber1.text.toString().toIntOrNull()?:0
+            val b = binding!!.etNumber2.text.toString().toIntOrNull()?:0
+            myViewModel.tru(a,b)
+        }
+        myViewModel.message.observe(this@MainActivity, Observer {message->
+            lifecycleScope.launch(Dispatchers.Main) {
+                binding!!.tvResult.text = message.toString()
+            }
+        })
+
     }
 }
--- app/src/main/java/com/hoan/coroutine/MyViewModel.kt
+++ app/src/main/java/com/hoan/coroutine/MyViewModel.kt
@@ -3,14 +3,26 @@ package com.hoan.coroutine
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MyViewModel : ViewModel() {
-   var _message = MutableLiveData<String>()
-
-    val message:LiveData<String>
+    val _message = MutableLiveData<Int>()
+    val message:LiveData<Int>
         get() = _message
 
-    fun updateText(v:String){
-        _message.value = v
+    fun cong(a:Int,b:Int){
+        viewModelScope.launch {
+            delay(5000L)
+            _message.postValue(a + b)
+        }
+    }
+
+    fun tru(a:Int,b:Int){
+        viewModelScope.launch {
+            delay(5000L)
+            _message.postValue(a - b)
+        }
     }
 }
